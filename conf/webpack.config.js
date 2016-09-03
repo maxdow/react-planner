436abe0ee2775648b8283a4484e6f5dbaff525cf
Maxime Warnier <marmax@gmail.com>
2016-09-03 20:32:43 +0200

    build for preview

--- conf/webpack.config.js
+++ conf/webpack.config.js
@@ -4,22 +4,18 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const loaders = require("./loaders.js");
 
 const sourceFolder = path.resolve(__dirname, "../src");
-const outputFolder = path.resolve(__dirname, "../dist");
+const outputFolder = path.resolve(__dirname, "../");
 const templateIndex = path.resolve(__dirname, "../src/index.html");
 
 
 module.exports = {
   devtool: "eval",
   entry: [
-    "webpack-dev-server/client?http://localhost:3000",
-    "webpack/hot/only-dev-server",
-    "react-hot-loader/patch",
     sourceFolder
   ],
   output: {
     path: outputFolder,
-    filename: "bundle.js",
-    publicPath: "http://localhost:3000/"
+    filename: "bundle.js"
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
