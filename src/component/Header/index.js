fa8db5fa26b821cc00071a20aea0c1def9302ffa
Maxime Warnier <marmax@gmail.com>
2016-09-03 20:20:43 +0200

    refactoring with grid-layout. Added drag and resize

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -1,27 +1,37 @@
 import React from "react"
+import ReactGridLayout,{WidthProvider} from 'react-grid-layout';
+const ReactGridLayoutw = WidthProvider(ReactGridLayout)
 
 import "./header.css"
 
 const aWeek = [0,1,2,3,4,5,6];
 
-export const DayList = ({startDate, currentDate}) => (
-  <div className="rpl-header-daylist">
-    {aWeek.map((day) => {
-      const newDate = startDate.clone().add(day,"d");
-      return  <div className={newDate.isSame(currentDate,"day") ? "rpl-header-daylist-day--current" : "rpl-header-daylist-day"} key={day}>
-              {newDate.format("dddd D")}
-              </div>
-    })}
-  </div>
-)
+export const DayList = ({startDate, currentDate}) => {
+  const layout = aWeek.map((day) => {
+    const newDate = startDate.clone().add(day,"d");
+    return {
+      i:day.toString(),
+      title:newDate.format("dddd D"),
+      isToday:newDate.isSame(currentDate,"day"),
+      x:day+1,
+      y:0,
+      w:1,
+      h:1,
+      static:true
+    }
 
-/*
-const Controls = () => (
+  })
+  return (
+  <ReactGridLayoutw className="layout" layout={layout} cols={8} rowHeight={36} margin={[0,0]} >
+    {layout.map(day => <div className={day.isToday ? "rpl-header-daylist-day rpl-header-daylist-day--current" : "rpl-header-daylist-day"} key={day.i}>{day.title}</div> )}
+  </ReactGridLayoutw>
+  )
+}
 
-)*/
 
 export const Header = ({startDate,currentDate,onWeekSub,onWeekAdd,style}) => (
   <div style={style} className="rpl-header">
+
     <div className="rpl-header-controls">
       <div>
       <button>-</button>
@@ -34,6 +44,8 @@ export const Header = ({startDate,currentDate,onWeekSub,onWeekAdd,style}) => (
       <button onClick={onWeekAdd}>+</button>
       </div>
     </div>
+
     <DayList startDate={startDate} currentDate={currentDate}/>
+
   </div>
 )
