8a50c0d317bc3c3ff6098c39a282413673c888d7
Maxime Warnier <marmax@gmail.com>
2016-09-06 10:55:23 +0200

    new colors

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ const node = document.getElementById("planner");
 moment.locale("fr");
 
 const keys = [
-  {id : 1,title:"module 1",color:"red"},
+  {id : 1,title:"module 1",color:"orange"},
   {id : 2,title:"module 2",color:"blue"},
   {id : 3,title:"module 3",color:"green"}
 ]
 const items = [
   {key:1,start:new Date(2016,7,29,9),end:new Date(2016,7,30,19),content:"Session 1 - Lieux 1 - Formateur 1"},
-  {key:1,start:new Date(2016,7,31,9),end:new Date(2016,8,2,19),content:"Session 2 - Lieux 1 - Formateur 1"},
+  {key:1,start:new Date(2016,7,31,9),end:new Date(2016,8,8,19),content:"Session 2 - Lieux 1 - Formateur 1"},
   {key:2,start:new Date(2016,7,30,9),end:new Date(2016,7,30,19)},
   {key:3,start:new Date(2016,7,31,9),end:new Date(2016,8,3,19)}
 ]
