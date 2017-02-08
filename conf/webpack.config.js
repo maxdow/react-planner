601985cbb7fa086108f9d21bc0412d19b5c0d36e
Maxime Warnier <marmax@gmail.com>
2017-02-08 16:24:08 +0100

    change dev port'

--- conf/webpack.config.js
+++ conf/webpack.config.js
@@ -11,7 +11,7 @@ const templateIndex = path.resolve(__dirname, "../src/index.html");
 module.exports = {
   devtool: "eval",
   entry: [
-    "webpack-dev-server/client?http://localhost:3000",
+    "webpack-dev-server/client?http://localhost:3333",
     "webpack/hot/only-dev-server",
     "react-hot-loader/patch",
     sourceFolder
@@ -19,7 +19,7 @@ module.exports = {
   output: {
     path: outputFolder,
     filename: "bundle.js",
-    publicPath: "http://localhost:3000/"
+    publicPath: "http://localhost:3333/"
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
